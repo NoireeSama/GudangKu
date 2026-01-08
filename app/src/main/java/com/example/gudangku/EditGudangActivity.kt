ad78d14eb89bd608f15e42dfe968128f5973fef9
mirzardy <mirza.ok0010@gmail.com>
2026-01-08 08:08:19 +0700

    fix edit gudang part 2

--- app/src/main/java/com/example/gudangku/EditGudangActivity.kt
+++ app/src/main/java/com/example/gudangku/EditGudangActivity.kt
@@ -3,7 +3,6 @@ package com.example.gudangku
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.launch
 
